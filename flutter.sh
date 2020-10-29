7371e240007b78db5d4db95e3affb89803a83f47
adhityarachmanh <53608645+adhityarachmanh@users.noreply.github.com>
2020-10-29 17:28:05 +0700

    Update flutter.sh

--- flutter.sh
+++ flutter.sh
@@ -76,7 +76,7 @@ CCreate(){
     CDB=""
     IFS='/' read -ra CTX <<< "$CONTEXT"
     if [ "${CONTEXT:$((${#CONTEXT}-1)):${#CONTEXT}}" == '/' ] || [ "${CONTEXT:0:1}" == '/' ]; then
-        echo -e "$CYELLOW [FILENAME] $CGREEN|$CYELLOW [DIR]{unlimited}/[FILENAME]$CRESET"
+        echo -e "$CYELLOW [FILENAME] $CGREEN|$CYELLOW [DIR]{infinity}/[FILENAME]$CRESET"
         return
     elif [ -f  ${FILENAME} ]; then
         echo -e "$CRED"$CONTEXT"."$TYPE".dart$CRESET$CYELLOW file already exists$CRESET."
