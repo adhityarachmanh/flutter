757507d5dba8586988a81788aa0a2052d6232a37
adhityarachmanh <53608645+adhityarachmanh@users.noreply.github.com>
2020-10-30 19:59:48 +0700

    Update index.dart

--- lib/index.dart
+++ lib/index.dart
@@ -1,17 +1,10 @@
 part of 'app.dart';
 
 class IndexController with ChangeNotifier {
-  AuthModel _auth;
   UserModel _user;
-  AuthModel get auth => _auth;
   UserModel get user => _user;
 
-  void userInfo(AuthModel payload) {
-    _auth = payload;
-    notifyListeners();
-  }
-
-  void authInfo(UserModel payload) {
+  void userInfo(UserModel payload) {
     _user = payload;
     notifyListeners();
   }
