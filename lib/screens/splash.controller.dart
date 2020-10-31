6f50c90444b0a4d1e51f4245ad4bb896e54cb5c3
adhityarachmanh <53608645+adhityarachmanh@users.noreply.github.com>
2020-10-31 15:37:51 +0700

    Update splash.controller.dart

--- lib/screens/splash.controller.dart
+++ lib/screens/splash.controller.dart
@@ -3,10 +3,4 @@ part of '../app.dart';
 class SplashController with ChangeNotifier {
     final String title = "SplashControllerWorks!";
 
-    void onInit() {
-        print("init SplashController");
-    } 
-    void onDestroy() {
-        print("destroy SplashController");
-    }
 }
