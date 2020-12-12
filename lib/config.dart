db62e2650eb76ae27b20db60eb83df1e32fc22fa
adhityarachmanh <53608645+adhityarachmanh@users.noreply.github.com>
2020-12-12 10:13:54 +0700

    Update config.dart

--- lib/config.dart
+++ lib/config.dart
@@ -3,9 +3,11 @@ part of 'app.dart';
 class Config {
   Map<String, dynamic> apiConfig = {
     "BASE": "res",
-    "API_URL": 'http://10.0.2.2:PORT',// AVD DEFAULT IP
-    "VERSION": "v1.0",
-    "FORMAT": "API_URL/VERSION/BASE"// FORMAT API
+    // CHANGE API URL | AVD DEFAULT LOCAL IP http://10.0.2.2:PORT
+    "API_URL": 'URL', 
+    "VERSION": "v1.0", 
+    // FORMAT API EX:https://example.com/v.1.0/res/{route}
+    "FORMAT": "VERSION/BASE"
   }; 
   String creator = 'CREATOR';
   String creatorName = "CREATOR_NAME";
