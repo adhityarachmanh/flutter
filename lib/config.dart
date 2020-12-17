107b201db39462109cde19d9209c931204d49e04
adhityarachmanh <53608645+adhityarachmanh@users.noreply.github.com>
2020-12-17 09:07:44 +0700

    Update config.dart

--- lib/config.dart
+++ lib/config.dart
@@ -3,6 +3,7 @@ part of 'app.dart';
 // CHANGE API URL | AVD DEFAULT LOCAL IP http://10.0.2.2:PORT
 // FORMAT API EX:https://example.com/v.1.0/res/{route}
 class Config {
+  String websocket = "ws://";
   Map<String, dynamic> apiConfig = {
     "BASE": "res",
     "API_URL": 'URL', 
